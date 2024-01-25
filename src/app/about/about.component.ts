5c6338a4b38ac8a743e92e8c6e56c45056d23245
houdini_077 <mohameddenna077@gmail.com>
2024-01-25 14:29:29 +0100

    hackernews api toegepast

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  standalone: true,
-  imports: [],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
 
+  topStories: any[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
+      .then(response => response.json())
+      .then((data: number[]) => {
+        console.log(data);
+        if (data && data.length > 0) {
+
+          const topStoryIds = data.slice(0, 5);
+          this.fetchStoryDetails(topStoryIds);
+        }
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }
+
+  fetchStoryDetails(storyIds: number[]) {
+    Promise.all(storyIds.map(storyId =>
+      fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json`)
+        .then(response => response.json())
+    ))
+    .then(stories => {
+      console.log(stories);
+      this.topStories = stories;
+    })
+    .catch(error => console.error('Error fetching story details:', error));
+  }
 }
